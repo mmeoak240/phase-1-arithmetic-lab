2b5d4040b3349c2ad5ae845562b08ac226b1558f
Michael Meoak <mmeoak240@gmail.com>
2022-02-23 09:05:41 -0700

    commit tree 201b3917d8eaf9ac492f80c843af48dc39e44cac

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+
+const num3 = 19;
+const num4 = 5;
+
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 19 % 5;
+
+let max = Math.max(5, 10, 15, 20);
+
+console.log(mod);
